1435d74a47b92a0de6f8307e3e24a2a3786b0ebd
Andre Cadet Tower <andre.cadet@epfl.ch>
2023-02-21 23:07:04 +0100

    === JUnit === + import de JUnit + ajout des méthodes à bits

--- src/ch/epfl/javions/Bits.java
+++ src/ch/epfl/javions/Bits.java
@@ -2,4 +2,11 @@ package ch.epfl.javions;
 
 public final class Bits {
     private Bits(){}
+
+    public static int extractUInt(long value, int start, int size){
+        if (size <= 0 || size > Integer.SIZE) throw new IllegalArgumentException();
+        if (start < 0 || start + size >= Long.SIZE) throw new IndexOutOfBoundsException();
+
+        return (int) value << (Long.SIZE - (start + size)) >>> (start + Long.SIZE - (start + size));
+    }
 }
