e237e96ed29bcf56e7c9d315ae18d760b3c41ed7
I-Azy-I <62987235+I-Azy-I@users.noreply.github.com>
2023-02-26 17:08:55 +0100

    === Crc bit à bit === + ca fonctionne en bit à bit

--- src/ch/epfl/javions/Crc24.java
+++ src/ch/epfl/javions/Crc24.java
@@ -2,8 +2,9 @@ package ch.epfl.javions;
 
 public final class Crc24 {
     public static final int GENERATOR = 0xFFF409;
+    private final int N = 24;
     private final int[] table;
-    private static final int N = 24;
+
 
     public Crc24(int generator){
         this.table = buildTable(generator);
@@ -12,7 +13,21 @@ public final class Crc24 {
     public static int crc_bitwiseTest(int generator, int value, int N){
         return crc_bitwise(generator, value, N);
     }
-    private static int crc_bitwise(int generator, int value, int N){
+    public static int crc_bitwise(int generator, byte[] value, int N) {
+        int crc = 0;
+        int[] table = {0, generator};
+        for (byte octet : value) {
+            for (int i = 7; i >= 0; i--) {
+                int b = Bits.testBit(octet, i)? 1 : 0;
+                crc = ((crc<<1) | b) ^ table[Bits.testBit(crc, N-1)? 1 : 0];
+            }
+        }
+        for (int i = 0; i < 24; i++) {
+            crc = ((crc<<1)) ^ table[Bits.testBit(crc, N-1)? 1 : 0];
+        }
+        return Bits.extractUInt(crc,0,N);
+    }
+    private static int crc_bitwise(int generator, long value, int N){
         //assert value <= 0xFF;
         value = value << N;
         int crc = 0;
@@ -35,18 +50,19 @@ public final class Crc24 {
     public int crc(byte[] bytes) {
         int crc = 0;
         for (byte octet : bytes) {
-            crc = ((crc<<8) | octet) ^ this.table[Bits.extractUInt(crc,N -8,8)];
+            crc = ((crc<<8) | octet) ^ this.table[Bits.extractUInt(crc,N-8,8)];
         }
         for (int i = 0; i < N / 8; i++) {
-            crc = (crc<<8) ^ this.table[Bits.extractUInt(crc,N - 1 -8,8)];
+            crc = (crc<<8) ^ this.table[Bits.extractUInt(crc,N-8,8)];
         }
         return Bits.extractUInt(crc,0,N);
 
     }
-    private static int[] buildTable(int generator){
+    private int[] buildTable(int generator){
         int[] table = new int[256];
         for (int i = 0; i < 256; i++) {
-            table[i] = crc_bitwise(generator, i, N);
+            byte[] value = {(byte)i};
+            table[i] = crc_bitwise(generator, value, N);
         }
         return table;
     }
--- test/ch/epfl/javions/Crc24Test.java
+++ test/ch/epfl/javions/Crc24Test.java
@@ -7,7 +7,23 @@ import java.util.HexFormat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Crc24Test {
+    @Test
+    void crcTestBitwiseBytes(){
+        var crc24 = new Crc24(Crc24.GENERATOR);
+        String[] list_mS ={"8D392AE499107FB5C00439", "8D4D2286EA428867291C08", "8D3950C69914B232880436", "8D4B17E399893E15C09C21", "8D4B18F4231445F2DB63A0", "8D495293F82300020049B8"};
+        String[] list_cS ={"035DB8", "EE2EC6", "BC63D3", "9FC014", "DEEB82", "111203"};
+        for (int i = 0; i < list_mS.length; i++) {
+            String mS = list_mS[i];
+            String cS = list_cS[i];
+            int c = Integer.parseInt(cS, 16); // == 0x035DB8
 
+            byte[] mAndC = HexFormat.of().parseHex(mS + cS);
+            assertEquals(0, crc24.crc_bitwise(Crc24.GENERATOR, mAndC, 24));
+
+            byte[] mOnly = HexFormat.of().parseHex(mS);
+            assertEquals(c, crc24.crc_bitwise(Crc24.GENERATOR, mOnly, 24));
+        }
+    }
     @Test
     void crcTestBitwise(){
         var crc24 = new Crc24(0b11001);
@@ -28,19 +44,25 @@ class Crc24Test {
         var expected3 = 0b1;
         assertEquals(expected3, actual31);
         assertEquals(0, actual32);
+
+
     }
     @Test
     void crc() {
         var crc24 = new Crc24(Crc24.GENERATOR);
-        String mS = "8D392AE499107FB5C00439";
-        String cS = "035DB8";
-        int c = Integer.parseInt(cS, 16); // == 0x035DB8
+        String[] list_mS ={"8D392AE499107FB5C00439", "8D4D2286EA428867291C08", "8D3950C69914B232880436", "8D4B17E399893E15C09C21", "8D4B18F4231445F2DB63A0", "8D495293F82300020049B8"};
+        String[] list_cS ={"035DB8", "EE2EC6", "BC63D3", "9FC014", "DEEB82", "111203"};
+        for (int i = 0; i < list_mS.length; i++) {
+            String mS = list_mS[i];
+            String cS = list_cS[i];
+            int c = Integer.parseInt(cS, 16); // == 0x035DB8
 
-        byte[] mAndC = HexFormat.of().parseHex(mS + cS);
-        assertEquals(0, crc24.crc(mAndC));
+            byte[] mAndC = HexFormat.of().parseHex(mS + cS);
+            assertEquals(0, crc24.crc(mAndC));
 
-        byte[] mOnly = HexFormat.of().parseHex(mS);
-        assertEquals(c, crc24.crc(mOnly));
+            byte[] mOnly = HexFormat.of().parseHex(mS);
+            assertEquals(c, crc24.crc(mOnly));
+        }
     }
 
 }
