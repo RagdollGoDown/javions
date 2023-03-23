aae2101f07d9601165a821cd9eeebff03c9be1e6
Andrecadet2002 <andre.cadet@epfl.ch>
2023-03-23 13:49:23 +0100

    === Test === + ajout des valeurs qu'on devrait utiliser pour tester

--- test/ch/epfl/javions/adsb/AircraftIdentificationMessageTest.java
+++ test/ch/epfl/javions/adsb/AircraftIdentificationMessageTest.java
@@ -62,36 +62,20 @@ class AircraftIdentificationMessageTest {
 
 
     private String[] AircraftIdentificationMessagesExpected = {
-            """
-            AircraftIdentificationMessage[
-             timeStampNs=1499146900,
-             icaoAddress=IcaoAddress[string=4D2228],
-             category=163,
-             callSign=CallSign[string=RYR7JD]]""",
-            """
-            AircraftIdentificationMessage[
-             timeStampNs=2240535600,
-             icaoAddress=IcaoAddress[string=01024C],
-             category=163,
-             callSign=CallSign[string=MSC3361]]""",
-            """
-            AircraftIdentificationMessage[
-             timeStampNs=2698727800,
-             icaoAddress=IcaoAddress[string=495299],
-             category=163,
-             callSign=CallSign[string=TAP931]]""",
-            """
-            AircraftIdentificationMessage[
-             timeStampNs=3215880100,
-             icaoAddress=IcaoAddress[string=A4F239],
-             category=165,
-             callSign=CallSign[string=DAL153]]""",
-            """
-            AircraftIdentificationMessage[
-             timeStampNs=4103219900,
-             icaoAddress=IcaoAddress[string=4B2964],
-             category=161,
-             callSign=CallSign[string=HBPRO]]"""
+            "AircraftIdentificationMessage[timeStampNs=1499146900, icaoAddress=IcaoAddress[string=4D2228], category=163, callSign=CallSign[string=RYR7JD]]",
+            "AircraftIdentificationMessage[timeStampNs=2240535600, icaoAddress=IcaoAddress[string=01024C], category=163, callSign=CallSign[string=MSC3361]]",
+            "AircraftIdentificationMessage[timeStampNs=2698727800, icaoAddress=IcaoAddress[string=495299], category=163, callSign=CallSign[string=TAP931]]",
+            "AircraftIdentificationMessage[timeStampNs=3215880100, icaoAddress=IcaoAddress[string=A4F239], category=165, callSign=CallSign[string=DAL153]]",
+            "AircraftIdentificationMessage[timeStampNs=4103219900, icaoAddress=IcaoAddress[string=4B2964], category=161, callSign=CallSign[string=HBPRO]]",
+            "AircraftIdentificationMessage[timeStampNs=4619251500, icaoAddress=IcaoAddress[string=4B1A00], category=162, callSign=CallSign[string=SAZ54]]",
+            "AircraftIdentificationMessage[timeStampNs=4750361000, icaoAddress=IcaoAddress[string=4D0221], category=162, callSign=CallSign[string=SVW29VM]]",
+            "AircraftIdentificationMessage[timeStampNs=5240059000, icaoAddress=IcaoAddress[string=4CA2BF], category=163, callSign=CallSign[string=RYR5907]]",
+            "AircraftIdentificationMessage[timeStampNs=6532434900, icaoAddress=IcaoAddress[string=4D2228], category=163, callSign=CallSign[string=RYR7JD]]",
+            "AircraftIdentificationMessage[timeStampNs=7009169300, icaoAddress=IcaoAddress[string=4D029F], category=161, callSign=CallSign[string=JFA17S]]",
+            "AircraftIdentificationMessage[timeStampNs=7157583600, icaoAddress=IcaoAddress[string=39CEAA], category=160, callSign=CallSign[string=TVF7307]]",
+            "AircraftIdentificationMessage[timeStampNs=7193198000, icaoAddress=IcaoAddress[string=4B17E5], category=163, callSign=CallSign[string=SWR6LH]]",
+            "AircraftIdentificationMessage[timeStampNs=7203089700, icaoAddress=IcaoAddress[string=4BCDE9], category=163, callSign=CallSign[string=SXS5GH]]",
+            "AircraftIdentificationMessage[timeStampNs=7306755600, icaoAddress=IcaoAddress[string=440237], category=163, callSign=CallSign[string=EJU63HA]]"
     };
 
 
@@ -110,11 +94,10 @@ class AircraftIdentificationMessageTest {
                         System.out.println(a);
                         //alors le test ne marche pas mais jsp si c'est à cause de comment ils mettent les textes pour le test
                         assertTrue(AircraftIdentificationMessage.of(m).toString()
-                                .equals(AircraftIdentificationMessagesExpected[1]));
+                                .equals(AircraftIdentificationMessagesExpected[i]));
                     }
             }
             System.out.println(i);
-
         }
     }
 }
--- test/ch/epfl/javions/adsb/CprDecoderTest.java
+++ test/ch/epfl/javions/adsb/CprDecoderTest.java
@@ -13,8 +13,30 @@ class CprDecoderTest {
         int x1 = 108865;
         int y1 = 77558;
         double N_bit = (1<<17);
-        GeoPos g = CprDecoder.decodePosition(x0/N_bit,y0/N_bit ,x1/N_bit, y1/N_bit, 0 );
-        System.out.println(g);
+
+
+        double[] expectedForRecent0 = {7.476062, 46.323349};
+        double[] expectedForRecent1 = {7.475166, 46.322363};
+
+        double[] actualForRecent0 ={
+                CprDecoder.decodePosition(x0/N_bit,y0/N_bit ,x1/N_bit, y1/N_bit, 0 )
+                        .longitudeDEGREE(),
+                CprDecoder.decodePosition(x0/N_bit,y0/N_bit ,x1/N_bit, y1/N_bit, 0 )
+                        .latitudeDEGREE()};
+        double[] actualForRecent1 ={
+                CprDecoder.decodePosition(x0/N_bit,y0/N_bit ,x1/N_bit, y1/N_bit, 1 )
+                        .longitudeDEGREE(),
+                CprDecoder.decodePosition(x0/N_bit,y0/N_bit ,x1/N_bit, y1/N_bit, 1 )
+                        .latitudeDEGREE()};
+
+        System.out.println(actualForRecent0[0]);
+        System.out.println(actualForRecent0[1]);
+
+        //lon 0
+        assertEquals(actualForRecent0[0] ,expectedForRecent0[0], Math.pow(10,-6));
+        assertEquals(actualForRecent0[1] ,expectedForRecent0[1], Math.pow(10,-6));
+        assertEquals(actualForRecent1[0] ,expectedForRecent1[0], Math.pow(10,-6));
+        assertEquals(actualForRecent1[1] ,expectedForRecent1[1], Math.pow(10,-6));
     }
 
     @Test
