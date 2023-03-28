e840f4bae9a53c6bb46f91ccd7802f60fb04c388
Andrecadet2002 <andre.cadet@epfl.ch>
2023-03-28 10:45:24 +0200

    === correction étape 5 === + fin state accumulateur + ajout test pour state accumulateur

--- src/ch/epfl/javions/adsb/AircraftStateAccumulator.java
+++ src/ch/epfl/javions/adsb/AircraftStateAccumulator.java
@@ -1,8 +1,60 @@
 package ch.epfl.javions.adsb;
 
-public class AircraftStateAccumulator <T>{
+import ch.epfl.javions.GeoPos;
+
+public class AircraftStateAccumulator <T extends AircraftStateSetter>{
+
+    private final static long TIME_BETWEEN_POSITIONS = 10000000000L;
+
+    private final T stateSetter;
+
+    private AirbornePositionMessage[] airbornePositions;
 
     public AircraftStateAccumulator(T stateSetter){
+        if (stateSetter == null){throw new NullPointerException();}
+        this.stateSetter = stateSetter;
 
+        airbornePositions = new AirbornePositionMessage[2];
+    }
+
+    public T stateSetter() {
+        return stateSetter;
+    }
+
+    public void update(Message message){
+        switch (message){
+            case AircraftIdentificationMessage aim ->
+                setStateOnIdentifierMessage(aim);
+            case AirbornePositionMessage apb ->
+                setStateOnPositionMessage(apb);
+            case AirborneVelocityMessage avm ->
+                setStateOnVelocityMessage(avm);
+            default -> System.out.println("unrecognized message");
+        }
+    }
+
+    private void setStateOnIdentifierMessage(AircraftIdentificationMessage aim){
+        stateSetter.setCategory(aim.category());
+        stateSetter.setCallSign(aim.callSign());
+    }
+
+    private void setStateOnPositionMessage(AirbornePositionMessage apb){
+        stateSetter.setAltitude(apb.altitude());
+
+        airbornePositions[apb.parity()] = apb;
+
+        if (airbornePositions[(apb.parity() + 1) % 2] != null
+                && airbornePositions[(apb.parity() + 1) % 2].timeStampNs() <= apb.timeStampNs() + TIME_BETWEEN_POSITIONS){
+
+            stateSetter.setPosition(CprDecoder.decodePosition(
+                    airbornePositions[0].x(), airbornePositions[0].y(),
+                    airbornePositions[1].x(), airbornePositions[1].y(),
+                    apb.parity()));
+        }
+    }
+
+    private void setStateOnVelocityMessage(AirborneVelocityMessage avm){
+        stateSetter.setVelocity(avm.speed());
+        stateSetter.setTrackOrHeading(avm.trackOrHeading());
     }
 }
