582240899f48f0095761a83663ad64b8ffc69de7
Andrecadet2002 <andre.cadet@epfl.ch>
2023-04-04 18:55:11 +0200

    === étape doc === + MessageParser

--- src/ch/epfl/javions/adsb/MessageParser.java
+++ src/ch/epfl/javions/adsb/MessageParser.java
@@ -1,29 +1,38 @@
 package ch.epfl.javions.adsb;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 /**
- * Takes a RawMessage and transforms it into
+ * Takes a RawMessage and transforms it into one of the message types that
+ * can be used to update the information on the plane
  */
 public class MessageParser {
 
-    private final static int[] AIRBORNE_VELOCITY_TYPECODE = {19};
-    private final static int[] AIRBORNE_POSITION_TYPECODE = {9,10,11,12,13,14,15,16,17,18,20,21,22};
-    private final static int[] AIRCRAFT_IDENTIFIER_TYPECODE = {1,2,3,4};
+    private final static HashSet<Integer> AIRBORNE_VELOCITY_TYPECODE = new HashSet<>(List.of(19));
+    private final static HashSet<Integer> AIRBORNE_POSITION_TYPECODE = new HashSet<>(List.of(9,10,11,12,13,14,15,16,17,18,20,21,22));
+    private final static HashSet<Integer> AIRCRAFT_IDENTIFIER_TYPECODE = new HashSet<>(List.of(1,2,3,4));
 
     private MessageParser(){}
 
-
+    /**
+     * From a RawMessage, the method will get the message type and returned a parsed version of it
+     * to find the type it uses the typeCode
+     * @param rawMessage the message to be parsed
+     * @return the parsed message
+     *          null if the RawMessage is invalid
+     */
     public static Message parse(RawMessage rawMessage){
         int typeCode = rawMessage.typeCode();
 
-        if (typeCode <= 4){
+        if (AIRBORNE_VELOCITY_TYPECODE.contains(typeCode)){
+            return AirborneVelocityMessage.of(rawMessage);
+        } else if (AIRCRAFT_IDENTIFIER_TYPECODE.contains(typeCode)){
             return AircraftIdentificationMessage.of(rawMessage);
+        } else if (AIRBORNE_POSITION_TYPECODE.contains(typeCode)) {
+            return AirbornePositionMessage.of(rawMessage);
         }
-        else if (typeCode >= 9 && typeCode <= 22){
-            return typeCode == 19 ?
-                    AirborneVelocityMessage.of(rawMessage) : AirbornePositionMessage.of(rawMessage);
-        }
-        else {
-            return null;
-        }
+        else {return null;}
     }
 }
