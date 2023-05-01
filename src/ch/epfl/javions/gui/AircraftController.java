37acab99827811348699bfea19a85517a85679e8
Andre Cadet Tower <andre.cadet@epfl.ch>
2023-05-01 22:15:42 +0200

    === étape 9 === + petite modif

--- src/ch/epfl/javions/gui/AircraftController.java
+++ src/ch/epfl/javions/gui/AircraftController.java
@@ -3,10 +3,13 @@ package ch.epfl.javions.gui;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.collections.ObservableSet;
+import javafx.collections.SetChangeListener;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.Pane;
 
 public final class AircraftController {
+
+    private final MapParameters mapParameters;
 
     private final Canvas canvas;
     private final Pane pane;
@@ -14,15 +17,29 @@ public final class AircraftController {
     private ObservableSet<ObservableAircraftState> aircraftStates;
     private ObjectProperty<ObservableAircraftState> followedAircraft;
 
-    public AircraftController(DoubleProperty widthProperty, DoubleProperty heightProperty,
+    public AircraftController(MapParameters mapParameters,
                               ObservableSet<ObservableAircraftState> aircraftStates,
                               ObjectProperty<ObservableAircraftState> followedAircraft){
+        this.mapParameters = mapParameters;
+
         canvas = new Canvas();
         pane = new Pane(canvas);
+        pane.setPickOnBounds(false);
         canvas.widthProperty().bind(pane.widthProperty());
         canvas.heightProperty().bind(pane.heightProperty());
 
         this.aircraftStates = aircraftStates;
+        aircraftStates.addListener((SetChangeListener<ObservableAircraftState>)
+                change -> { /* … corps de la lambda */ });
         this.followedAircraft = followedAircraft;
+    }
+
+    public Pane getPane(){
+        //TODO demander s'il faut que ce soit une 'copie' de pane
+        return pane;
+    }
+
+    private void icon(){
+
     }
 }
